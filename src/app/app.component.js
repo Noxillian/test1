adb69316b292e9cbb043e70fe95b63490cc46b2f
Dustin Leese <dleese@mevango.com>
2017-03-22 14:57:21 +0100

    gg2

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -11,12 +11,12 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.name = '1';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Hello {{name}}</h1>"
+            template: "<h1>test {{name}}</h1>"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
